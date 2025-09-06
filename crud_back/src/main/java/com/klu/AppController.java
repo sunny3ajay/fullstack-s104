77d30775830f9e2456395672073ba3fed78472d1
sunny3ajay <ajayradha9989@gmail.com>
2025-09-06 10:18:56 +0530

    Update AppController.java

--- crud_back/src/main/java/com/klu/AppController.java
+++ crud_back/src/main/java/com/klu/AppController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin
 public class AppController {
 	@Autowired
 	Service s;
